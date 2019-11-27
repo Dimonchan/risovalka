f18393bb15ee777a1ba715185c392757d80e243b
Dimonchan <58161739+Dimonchan@users.noreply.github.com>
2019-11-27 14:26:41 +0300

    initial commit

---
+++ risovalka — копия/risovalka/Brush.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace risovalka
+{
+    public class Brush
+    {
+        Color color=Color.Black;
+        public Bitmap bitMapImage;
+
+
+        public Brush(Bitmap b)
+        {
+            bitMapImage = b;
+        }
+
+        public Brush(Color color)
+        {
+            this.color = color;
+        }
+
+        public Bitmap DrawLine(int x1, int y1, int x2, int y2)
+        {
+            double speed;
+
+            double h = y2 - y1, w = x2 - x1;
+
+            if (Math.Abs(h) > Math.Abs(w))
+            {
+                speed = h != 0 ? w / h : 10;
+                if (h >= 0)
+                {
+                    for (int i = 0; i <= h; i++)
+                    {
+                        int j = Convert.ToInt32(i * speed);
+                        bitMapImage.SetPixel(x1 + j, y1 + i, color);
+                    }
+                }
+                else
+                {
+                    for (int i = Convert.ToInt32(h); i <= 0; i++)
+                    {
+                        int j = Convert.ToInt32(i * speed);
+                        bitMapImage.SetPixel(x1 + j, y1 + i, color);
+                    }
+                }
+
+            }
+            else
+            {
+                speed = w != 0 ? h / w : 10;
+                if (w >= 0)
+                {
+                    for (int i = 0; i <= w; i++)
+                    {
+                        int j = Convert.ToInt32(i * speed);
+                        bitMapImage.SetPixel(x1 + i, y1 + j, color);
+                    }
+                }
+                else
+                {
+                    for (int i = Convert.ToInt32(w); i <= 0; i++)
+                    {
+                        int j = Convert.ToInt32(i * speed);
+                        bitMapImage.SetPixel(x1 + i, y1 + j, color);
+                    }
+                }
+
+            }
+
+            return bitMapImage;
+        }
+
+
+
+        public void DrawRectangle(int x1, int y1, int x2, int y2)
+        {
+
+
+            DrawLine(x1,y1,x2,y1);
+            DrawLine(x1,y2,x2,y2);
+            DrawLine(x1,y1,x1,y2);
+            DrawLine(x2,y1,x2,y2);
+
+
+
+
+
+
+        }
+
+        public void DrawSquare(int x1, int y1, int x2, int y2)
+        {
+
+            int a = x2 - x1;
+            y2 = y1 + a;
+            DrawLine(x1, y1, x2, y1);
+            DrawLine(x2, y1, x2, y2);
+            DrawLine(x2, y2, x1, y2);
+            DrawLine(x1, y2, x1, y1);
+
+
+        }
+
+        public void DrawRavnoBedrTr(int x1, int y1, int x2, int y2)
+        {
+            int x3 = 2 * x1 - x2;
+            DrawLine(x1, y1, x2, y2);
+            DrawLine(x2, y2, x3, y2);
+            DrawLine(x3, y2, x1, y1);
+        }
+
+        public void PryamoiTr(int x1, int y1, int x2, int y2)
+        {
+            
+            DrawLine(x1, y1, x2, y2);
+            DrawLine(x2, y2, x1, y2);
+            DrawLine(x1, y2, x1, y1);
+
+            
+        }
+
+        public void RavnoStoron(int x1, int y1, int x2, int y2)
+        {
+
+
+            int r = Math.Abs(x2 - x1);
+            int x3 = x1 + r / 2;
+            int a = r / 2;
+            int b = Convert.ToInt32(Math.Sqrt(r*r-a*a));
+            int y3 = y1 - b;
+
+            DrawLine(x1, y1, x2, y1);
+            DrawLine(x2, y1, x3, y3);
+            DrawLine(x3, y3, x1, y1);
+
+        }
+
+        public void DrawCircle(int x1, int y1, int x2, int y2)
+        {
+            double x = Math.Abs(x2 - x1);
+            double y = Math.Abs(y2 - y1);
+            int r = Convert.ToInt32(Math.Sqrt(x*x + y*y));
+
+            int xt = 0, yt =r;
+
+            for (int i = 0; i <= r; i++)
+            {
+                double a = Math.Sqrt(r*r - i*i);
+                
+
+                
+                DrawLine(xt + x1, yt + y1, i + x1, Convert.ToInt32(a) + y1);
+                DrawLine(-xt + x1, yt + y1, -i + x1, Convert.ToInt32(a) + y1);
+                DrawLine(xt + x1, -yt + y1, i + x1, -Convert.ToInt32(a) + y1);
+                DrawLine(-xt + x1, -yt + y1, -i + x1,- Convert.ToInt32(a) + y1);
+                xt = i;
+                yt = Convert.ToInt32(a);
+
+            }
+
+
+        }
+
+
+
+
+        /*
+        
+        public Bitmap DrawSquare(int x1, int y1, int x2, int y2)
+        {
+            double h = y2 - y1, w = x2 - x1;
+
+            double side;
+
+            if (h > w)
+            {
+                side = h;
+            } else
+            {
+                side = w;
+            }
+
+            for (int i = 0; i <= side; i++)
+            {
+                int j = Convert.ToInt32(i);
+                    bitMapImage.SetPixel(x1 + i, y1, color);
+            }
+
+
+            return bitMapImage;
+            
+
+        }
+        */
+    }
+}
---
+++ risovalka — копия/risovalka/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace risovalka
+{
+
+    public partial class Form1 : Form
+    {
+        Bitmap bitMapImage;
+        Bitmap bbb;
+        bool FmouseLeft = false;
+        bool RectDraw = false;
+        bool SquareDraw = false;
+        bool CircleDraw = false;
+        bool TriangleDraw = false;
+        bool ColorRed = false;
+        bool FatDraw = false;
+
+
+        int x, y;
+      
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (FmouseLeft && e.Location.X < pictureBox1.Width && e.Location.Y < pictureBox1.Height
+                && e.Location.X >= 0 && e.Location.Y >= 0)
+            {
+
+                if (RectDraw)
+                {
+                    Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                    bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                    Brush b = new Brush(bbb);
+                    b.DrawRectangle(x, y, e.X, e.Y);
+
+                    pictureBox1.Image = b.bitMapImage;
+
+                }
+                else if (SquareDraw)
+                {
+                    Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                    bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                    Brush b = new Brush(bbb);
+                    b.DrawSquare(x, y, e.X, e.Y);
+
+                    pictureBox1.Image = b.bitMapImage;
+                }
+
+                else if (CircleDraw)
+                {
+                    Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                    bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                    Brush b = new Brush(bbb);
+                    b.DrawCircle(x, y, e.X, e.Y);
+
+                    pictureBox1.Image = b.bitMapImage;
+                }
+
+                else if (TriangleDraw)
+                {
+                    if (Control.ModifierKeys == Keys.Shift)
+                    {
+                        Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                        bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                        Brush b = new Brush(bbb);
+                        b.PryamoiTr(x, y, e.X, e.Y);
+
+                        pictureBox1.Image = b.bitMapImage;
+                    }
+                    else if (Control.ModifierKeys ==Keys.Control)
+                    {
+                        Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                        bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                        Brush b = new Brush(bbb);
+                        b.DrawRavnoBedrTr(x, y, e.X, e.Y);
+
+                        pictureBox1.Image = b.bitMapImage;
+
+
+                    }
+                    else
+                    {
+                        Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                        bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                        Brush b = new Brush(bbb);
+                        b.RavnoStoron(x, y, e.X, e.Y);
+
+                        pictureBox1.Image = b.bitMapImage;
+                    }
+
+                }
+
+                else
+                {
+
+                    if (FatDraw)
+                    {
+                        Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                        bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                        Brush b = new Brush(bitMapImage);
+
+                        //b.DrawCircle(x, y, e.X + 1, e.Y + 1);
+                        //b.DrawCircle(x, y, e.X + 2, e.Y + 2);
+                        //b.DrawCircle(x, y, e.X + 3, e.Y + 3);
+                        x = e.X;
+                        y = e.Y;
+                        b.DrawCircle(x, y, e.X, e.Y);
+                        b.DrawCircle(x, y, e.X+4, e.Y+4);
+                        pictureBox1.Image = b.bitMapImage;
+                    }
+                    else
+                    {
+                        Rectangle r = new Rectangle(0, 0, pictureBox1.Width - 1, pictureBox1.Height - 1);
+                        bbb = bitMapImage.Clone(r, System.Drawing.Imaging.PixelFormat.DontCare);
+                        Brush b = new Brush(bitMapImage);
+                        b.DrawLine(x, y, e.Location.X, e.Location.Y);
+                        x = e.X;
+                        y = e.Y;
+                        
+                        pictureBox1.Image = b.bitMapImage;
+                    }
+
+                }
+
+
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bitMapImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bitMapImage;
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            FmouseLeft = true;
+            this.x = e.X;
+            this.y = e.Y;
+
+        }
+
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            FmouseLeft = false;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Rectangle_Click(object sender, EventArgs e)
+        {
+            RectDraw = true;
+            SquareDraw = false;
+            CircleDraw = false;
+            TriangleDraw = false;
+
+        }
+
+        private void Square_Click(object sender, EventArgs e)
+        {
+            SquareDraw = true;
+            RectDraw = false;
+            CircleDraw = false;
+            TriangleDraw = false;
+
+        }
+
+        private void Circle_Click(object sender, EventArgs e)
+        {
+            CircleDraw = true;
+            RectDraw = false;
+            SquareDraw = false;
+            TriangleDraw = false;
+
+        }
+
+        private void Brush_Click(object sender, EventArgs e)
+        {
+            
+            RectDraw = false;
+            SquareDraw = false;
+            CircleDraw = false;
+            TriangleDraw = false;
+        }
+
+
+
+
+
+        private void Red_Click(object sender, EventArgs e)
+        {
+            //Brush b = new Brush(Color.Red);
+            //ColorRed = true;
+        }
+
+        private void Triangle_Click(object sender, EventArgs e)
+        {
+            TriangleDraw = true;
+            RectDraw = false;
+            SquareDraw = false;
+            CircleDraw = false;
+        }
+
+        private void Fat_Click(object sender, EventArgs e)
+        {
+            FatDraw = true;
+            RectDraw = false;
+            SquareDraw = false;
+            CircleDraw = false;
+            TriangleDraw = false;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            FmouseLeft = false;
+            bitMapImage = bbb;
+        }
+    }
+}
