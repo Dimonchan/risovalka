2e28d1e2ccfb92fbcc23e94a64f30c2c6536f7aa
Dimonchan <58161739+Dimonchan@users.noreply.github.com>
2019-11-27 14:26:41 +0300

    added

---
+++ risovalka — копия/risovalka/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace risovalka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.Fat = new System.Windows.Forms.Button();
+            this.Triangle = new System.Windows.Forms.Button();
+            this.Red = new System.Windows.Forms.Button();
+            this.Brush = new System.Windows.Forms.Button();
+            this.Circle = new System.Windows.Forms.Button();
+            this.Square = new System.Windows.Forms.Button();
+            this.Rectangle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(889, 507);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Fat);
+            this.panel1.Controls.Add(this.Triangle);
+            this.panel1.Controls.Add(this.Red);
+            this.panel1.Controls.Add(this.Brush);
+            this.panel1.Controls.Add(this.Circle);
+            this.panel1.Controls.Add(this.Square);
+            this.panel1.Controls.Add(this.Rectangle);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(889, 92);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Fat
+            // 
+            this.Fat.Location = new System.Drawing.Point(285, 31);
+            this.Fat.Name = "Fat";
+            this.Fat.Size = new System.Drawing.Size(75, 23);
+            this.Fat.TabIndex = 6;
+            this.Fat.Text = "Fat";
+            this.Fat.UseVisualStyleBackColor = true;
+            this.Fat.Click += new System.EventHandler(this.Fat_Click);
+            // 
+            // Triangle
+            // 
+            this.Triangle.Location = new System.Drawing.Point(94, 3);
+            this.Triangle.Name = "Triangle";
+            this.Triangle.Size = new System.Drawing.Size(75, 23);
+            this.Triangle.TabIndex = 2;
+            this.Triangle.Text = "Triangle";
+            this.Triangle.UseVisualStyleBackColor = true;
+            this.Triangle.Click += new System.EventHandler(this.Triangle_Click);
+            // 
+            // Red
+            // 
+            this.Red.BackColor = System.Drawing.Color.Red;
+            this.Red.Location = new System.Drawing.Point(582, 14);
+            this.Red.Name = "Red";
+            this.Red.Size = new System.Drawing.Size(39, 26);
+            this.Red.TabIndex = 5;
+            this.Red.UseVisualStyleBackColor = false;
+            this.Red.Click += new System.EventHandler(this.Red_Click);
+            // 
+            // Brush
+            // 
+            this.Brush.Location = new System.Drawing.Point(94, 32);
+            this.Brush.Name = "Brush";
+            this.Brush.Size = new System.Drawing.Size(75, 23);
+            this.Brush.TabIndex = 3;
+            this.Brush.Text = "Brush";
+            this.Brush.UseVisualStyleBackColor = true;
+            this.Brush.Click += new System.EventHandler(this.Brush_Click);
+            // 
+            // Circle
+            // 
+            this.Circle.Location = new System.Drawing.Point(13, 61);
+            this.Circle.Name = "Circle";
+            this.Circle.Size = new System.Drawing.Size(75, 23);
+            this.Circle.TabIndex = 2;
+            this.Circle.Text = "Circle";
+            this.Circle.UseVisualStyleBackColor = true;
+            this.Circle.Click += new System.EventHandler(this.Circle_Click);
+            // 
+            // Square
+            // 
+            this.Square.Location = new System.Drawing.Point(13, 32);
+            this.Square.Name = "Square";
+            this.Square.Size = new System.Drawing.Size(75, 23);
+            this.Square.TabIndex = 1;
+            this.Square.Text = "Square";
+            this.Square.UseVisualStyleBackColor = true;
+            this.Square.Click += new System.EventHandler(this.Square_Click);
+            // 
+            // Rectangle
+            // 
+            this.Rectangle.Location = new System.Drawing.Point(13, 3);
+            this.Rectangle.Name = "Rectangle";
+            this.Rectangle.Size = new System.Drawing.Size(75, 23);
+            this.Rectangle.TabIndex = 0;
+            this.Rectangle.Text = "Rectangle";
+            this.Rectangle.UseVisualStyleBackColor = true;
+            this.Rectangle.Click += new System.EventHandler(this.Rectangle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(888, 508);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button Rectangle;
+        private System.Windows.Forms.Button Square;
+        private System.Windows.Forms.Button Circle;
+        private System.Windows.Forms.Button Brush;
+        private System.Windows.Forms.Button Red;
+        private System.Windows.Forms.Button Triangle;
+        private System.Windows.Forms.Button Fat;
+    }
+}
+
